45c36ced4d546f4537eac71dbc2c379e3167599c
Vadym Kostyrkin <kiristonk@gmail.com>
2020-11-08 10:06:50 +0100

    task2 in  progress

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const session = require('express-session')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+
+var data = {}
+
+app.use(session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: 'keyboard cat'
+}));
+
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -12,6 +27,25 @@ app.post('/soap', (req, res) => {
 
 app.post('/rest', (req, res) => {
     res.send('rest')
+})
+
+app.get('/info', (req, res) => {
+    res.send({author: 24055})
+})
+
+app.get('/400', (req,res,next) => {
+    res.send('Error! Name must be in range of 1 and 10 characters and contain only letters betweenn a-z.', 400);
+})
+
+app.post('/store', urlencodedParser, (req, res) => {
+    users.
+    res.send(req.body);
+})
+
+app.get('/hello/:name', (req, res) => {
+    const regEx = /^([a-z]){1,10}$/gi;
+    if (!regEx.test(req.params.name)) res.redirect('/400');
+    else res.send('Hello, ' + req.params.name, 200)
 })
 
 app.listen(port, () => {
